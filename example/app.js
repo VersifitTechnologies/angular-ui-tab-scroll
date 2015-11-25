dbbf067c214daf0d056ce14179e9620bfe905fa6
Shemesh <shemeshi@gmail.com>
2015-11-25 16:05:41 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -6,12 +6,31 @@ app.config(function(scrollableTabsetConfigProvider){
     scrollableTabsetConfigProvider.setTooltipRight('left');
 })
 
-app.controller('MainCtrl', function($scope) {
-  $scope.tabs = [];
-  for(var i=0; i<25; i++) {
-    $scope.tabs.push({
+app.controller('MainCtrl', ['$scope', function($scope) {
+  var vm = this;
+  vm.tabs = [];
+  vm.scrlTabsApi = null;
+
+  vm.reCalcScroll = function() {
+    vm.scrlTabsApi.doRecalculate();
+  };
+
+  vm.addTab = function(){
+    vm.tabs.push({
+      heading: 'New Tab ' + vm.tabs.length,
+      content: 'This is the content for a NEW tab ' + vm.tabs.length
+    });
+  };
+
+  vm.removeTab = function(){
+      vm.tabs.splice(vm.tabs.length - 1, 1);
+  };
+
+  for(var i=0; i<5; i++) {
+    vm.tabs.push({
       heading: 'Tab ' + i,
       content: 'This is the content for tab ' + i
     });
-  }
-});
+  };
+
+}]);
