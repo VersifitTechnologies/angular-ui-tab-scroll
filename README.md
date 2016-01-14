1a153fd23d9a2a7d7de77f8e95a2aa941557b41c
Shemesh <shemeshi@gmail.com>
2016-01-14 23:21:47 +0200

    Major version
    - added a drop-down to show and activate tabs.
    - added data-tabScrollIgnore and data-tabScrollHeading.
    - removed header selector in favor of more efficient textContent.
    - added auto-recalculate, use with caution!
    - use native scrollIntoView().
    - fixed bugs in code and css.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A scrollable tab plugin intended for scrolling UI Bootstrap [tabset](https://ang
 
 Here is a working demo : http://plnkr.co/edit/YJNDaQ?p=preview
 
+[![npm version](https://img.shields.io/npm/v/angular-ui-tab-scroll.svg?style=flat-square)](https://www.npmjs.com/package/angular-ui-tab-scroll)
+[![npm downloads](https://img.shields.io/npm/dm/angular-ui-tab-scroll.svg?style=flat-square)](http://npm-stat.com/charts.html?package=angular-ui-tab-scroll&from=2015-01-01)
+![License](https://img.shields.io/badge/License-MIT-blue.svg?style=flat-square)
+
 ### Usage
 * Include `angular-ui-tab-scroll.js` and `angular-ui-tab-scroll.css` in your page.
 * Add `ui.tab.scroll` to your angular module dependencies.
@@ -18,14 +22,19 @@ Here is a working demo : http://plnkr.co/edit/YJNDaQ?p=preview
 </scrollable-tabset>
 ```
 
-### Options
-* `show-tooltips` - whether or not to show the side-tooltips. default is `true`.
-* `tooltip-left-placement` - which tooltip direction to use for the left tooltip (bottom, top, left, right). default is `right`.
-* `tooltip-right-placement` - which tooltip direction to use for the right tooltip (bottom, top, left, right). default is `left`.
-* `scroll-by` - the amount of pixels to offset upon each scroll. default is `50`
-* `tooltip-text-selector` - the selector for your tooltips. default is `tab-heading`
+### Attributes
+* `show-drop-down` - whether or not to show the drop-down for navigating the tabs, the drop-down reflects the selected tab and reflect if a tab is disabled.  default is `true`.
+* `show-tooltips` - whether or not to show tooltips on the scroll buttons. default is `true`.
+* `tooltip-left-placement` - which tooltip direction to use for the left button tooltip (bottom, top, left, right). default is `right`.
+* `tooltip-right-placement` - which tooltip direction to use for the right button tooltip (bottom, top, left, right). default is `left`.
+* `scroll-by` - the amount of pixels to offset upon each scroll. default is `50`.
+* `auto-recalculate` - whether or not to watch the tabs collection for changes to initiate a re-calculation. default is `false`. important! see warning below
 
-These options can directly be set on each directive as **DOM attributes**. 
+And additional attributes you can set on an individual tab:
+* `data-tabScrollIgnore` - if there is 'data-tabScrollIgnore="true"' on a tab than it will not be shown in the drop-down.
+* `data-tabScrollHeading` - put this on a tab and the value of it will be the text for this tab's tooltip and drop-down item.
+
+Attributes can be directly set on each directive as DOM attributes
 
 Example:
 ```html
@@ -52,7 +61,10 @@ angular.module('yourApp', [])
 
 This way, you can keep the directive usage simple and consistent across all your html.
 
-> **Important Note:** When an option is both defined at directive level and at config level, the value specified in the DOM **takes precedence over the one from the config**!.
+> **Important Notes:**
+* Use `auto-recalculate` with caution! - when set to true a watcher is added to the collection of tabs, and watcher are costly on performance! it is better to call `doRecalculate()` when needed. use this option only on small applications.
+* When an option is both defined at directive level and at config level, the value specified in the DOM takes precedence over the one from the config!.
+
 
 ### Api
 there is an exposed api, with it you can call:
@@ -66,7 +78,7 @@ both files are the result of a transpiled scss, which are also included in this 
 
 if you intend to have your own design i highly recommend you start with `angular-ui-tab-scroll-flat.scss` with it you can unleash the power of scss&css.
 
-to change the icons on the scroll buttons you simply need to override the relevant button's css with your own css
+to change the icons on the buttons you simply need to override the relevant button's css with your own css
 
 Example:
 ```css
--- angular-ui-tab-scroll.js
+++ angular-ui-tab-scroll.js
@@ -2,7 +2,7 @@
  * angular-ui-tab-scroll
  * https://github.com/VersifitTechnologies/angular-ui-tab-scroll
  *
- * Version: 1.3.1
+ * Version: 2.0.0
  * License: MIT
  */
 
@@ -11,22 +11,22 @@ angular.module('ui.tab.scroll', [])
 
           //the default options
           var defaultConfig = {
+            showDropDown: true,
             showTooltips: true,
             tooltipLeftPlacement: 'right',
             tooltipRightPlacement: 'left',
             scrollBy: '50',
-
-            //select the innermost child that isn't a span
-            //this way we cover getting <tab-heading> and <tab heading=''>
-            //but leave other markup out of it, unless it's a span (commonly an icon)
-            tabHeaderSelector: 'tab-heading'
+            autoRecalculate: false
           };
 
           var config = angular.extend({}, defaultConfig);
 
           return {
+            setShowDropDown : function(value){
+              config.showDropDown = (value == true);
+            },
             setShowTooltips : function(value){
-              config.showTooltips = value;
+              config.showTooltips = (value == true);
             },
             setTooltipLeftPlacement : function(value){
               config.tooltipLeftPlacement = value;
@@ -37,16 +37,17 @@ angular.module('ui.tab.scroll', [])
             setScrollBy : function(value){
               config.scrollBy = value;
             },
-            setTabHeaderSelector : function(value){
-              config.tabHeaderSelector = value;
+            setAutoRecalculate : function(value){
+              config.autoRecalculate = (value == true);
             },
             $get: function(){
               return {
+                showDropDown: config.showDropDown,
                 showTooltips: config.showTooltips,
                 tooltipLeftPlacement: config.tooltipLeftPlacement,
                 tooltipRightPlacement: config.tooltipRightPlacement,
                 scrollBy: config.scrollBy,
-                tabHeaderSelector: config.tabHeaderSelector
+                autoRecalculate: config.autoRecalculate
               };
             }
           };
@@ -61,11 +62,12 @@ angular.module('ui.tab.scroll', [])
           transclude: true,
 
           scope: {
+            showDropDown: '@',
             showTooltips: '@',
             tooltipLeftPlacement: '@',
             tooltipRightPlacement: '@',
             scrollBy: '@',
-            tabHeaderSelector: '@',
+            autoRecalculate: '@',
             api: '='
           },
 
@@ -83,18 +85,27 @@ angular.module('ui.tab.scroll', [])
           }],
 
           template: [
-            '<div class="ui-tabs-scrollable">',
-            '<button type="button" ng-hide="hideButtons" ng-disabled="disableLeft" class="btn nav-button left-nav-button" tooltip-placement="{{tooltipLeftDirection}}" tooltip-html="tooltipLeftHtml">',
-            '</button>',
-            '<div class="spacer" ng-class="{\'hidden-buttons\': hideButtons}" ng-transclude></div>',
-            '<button type="button" ng-hide="hideButtons" ng-disabled="disableRight" class="btn nav-button right-nav-button" tooltip-placement="{{tooltipRightDirection}}" tooltip-html="tooltipRightHtml">',
-            '</button>',
+            '<div class="ui-tabs-scrollable" ng-class="{\'show-drop-down\': !hideDropDown}">',
+              '<button type="button" ng-hide="hideButtons" ng-disabled="disableLeft" class="btn nav-button left-nav-button" tooltip-placement="{{tooltipLeftDirection}}" tooltip-html="tooltipLeftHtml"></button>',
+              '<div class="spacer" ng-class="{\'hidden-buttons\': hideButtons}" ng-transclude></div>',
+              '<button type="button" ng-hide="hideButtons" ng-disabled="disableRight" class="btn nav-button right-nav-button" tooltip-placement="{{tooltipRightDirection}}" tooltip-html="tooltipRightHtml"></button>',
+              '<div class="btn-group" dropdown is-open="isDropDownOpen" ng-hide="hideDropDown">',
+                '<button type="button" class="btn" dropdown-toggle></button>',
+                '<ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="single-button">',
+                  '<li role="menuitem" ng-repeat="tab in dropdownTabs" ng-class="{\'disabled\': tab.disabled}" ng-click="activateTab(tab)">',
+                    '<a href="#"><span class="dropDownTabActiveMark" ng-style="{\'visibility\': tab.active?\'visible\':\'hidden\'}"></span>{{tab.tabScrollTitle}}</a>',
+                  '</li>',
+                '</ul>',
+              '</div>',
             '</div>'
           ].join(''),
 
           link: function($scope, $el) {
 
+            $scope.dropdownTabs = [];
+            $scope.isDropDownOpen = false;
             $scope.hideButtons = true;
+            $scope.hideDropDown = true;
             $scope.tooltipRightHtml = '';
             $scope.tooltipLeftHtml = '';
             $scope.disableLeft = true;
@@ -103,7 +114,8 @@ angular.module('ui.tab.scroll', [])
             $scope.tooltipRightDirection =  $scope.tooltipRightPlacement ? $scope.tooltipRightPlacement : scrollableTabsetConfig.tooltipRightPlacement;
 
             var mouseDownInterval = null;
-            var showTooltips = $scope.showTooltips ? $scope.showTooltips === 'true' : scrollableTabsetConfig.showTooltips;
+            var showDropDown = $scope.showDropDown ? $scope.showDropDown === 'true' : scrollableTabsetConfig.showDropDown;
+            var showTooltips = $scope.showTooltips ? $scope.showTooltips === 'true' : scrollableTabsetConfig.showTooltips == true;
 
             var cancelId = function() {
               if(mouseDownInterval) {
@@ -145,17 +157,6 @@ angular.module('ui.tab.scroll', [])
               element.on('mouseup', mouseUp);
             };
 
-            var getTabHeaderFromTab = function(node) {
-              var selector = $scope.tabHeaderSelector ? $scope.tabHeaderSelector : scrollableTabsetConfig.tabHeaderSelector;
-              var nodeSelector = node.querySelector(selector);
-              if(nodeSelector) {
-                var html = angular.element(nodeSelector).html().trim();
-                if (html) {
-                  return html;
-                }
-              }
-            };
-
             var onWindowResize = function() {
               $scope.reCalcAll();
               $scope.$apply();
@@ -167,6 +168,14 @@ angular.module('ui.tab.scroll', [])
                 $scope.reCalcSides();
               };
             };
+
+            $scope.activateTab = function(tab) {
+              if(tab.disabled)return;
+              tab.active = true;
+              $timeout(function () {
+                $scope.scrollTabIntoView();
+              });
+            }
 
             $scope.reCalcSides = function() {
               if(!$scope.tabContainer || $scope.hideButtons)return;
@@ -185,17 +194,22 @@ angular.module('ui.tab.scroll', [])
 
               var allTabs = $scope.tabContainer.querySelectorAll('li');
               angular.forEach(allTabs, function(tab) {
-                var tabHeader = getTabHeaderFromTab(tab);
 
                 var rightPosition = parseInt(tab.getBoundingClientRect().left + tab.getBoundingClientRect().width - $scope.tabContainer.getBoundingClientRect().left);
                 var leftPosition = tab.getBoundingClientRect().left - $scope.tabContainer.getBoundingClientRect().left;
+                var heading = tab.getAttribute("data-tabScrollHeading");
+                var ignore = tab.getAttribute("data-tabScrollIgnore");
 
-                if(rightPosition > $scope.tabContainer.offsetWidth ) {
-                  if (tabHeader)rightTooltips.push(tabHeader.split(' ').join('&nbsp;'));
+                if(rightPosition > $scope.tabContainer.offsetWidth && !ignore ) {
+                  if(heading) {
+                    rightTooltips.push(heading)
+                  } else if (tab.textContent)rightTooltips.push(tab.textContent);
                 }
 
-                if (leftPosition < 0 ) {
-                  if (tabHeader)leftTooltips.push(tabHeader.split(' ').join('&nbsp;'));
+                if (leftPosition < 0 && !ignore ) {
+                  if(heading) {
+                    leftTooltips.push(heading)
+                  } else if (tab.textContent)leftTooltips.push(tab.textContent);
                 }
 
               });
@@ -215,14 +229,14 @@ angular.module('ui.tab.scroll', [])
               if(argInt) { // scroll tab index into view
                 var allTabs = $scope.tabContainer.querySelectorAll('li');
                 if(allTabs.length > argInt) { // only if its really exist
-                  $scope.tabContainer.scrollLeft = allTabs[argInt].offsetLeft - ($scope.tabContainer.clientWidth / 2) - 25;
+                  allTabs[argInt].scrollIntoView();
                 }
               }
 
               else { // scroll selected tab into view
                 var activeTab = $scope.tabContainer.querySelector('li.active');
                 if(activeTab) {
-                  $scope.tabContainer.scrollLeft = activeTab.offsetLeft - ($scope.tabContainer.clientWidth / 2) - 25;
+                  activeTab.scrollIntoView();
                 }
               }
 
@@ -231,8 +245,23 @@ angular.module('ui.tab.scroll', [])
 
             // init is called only once!
             $scope.init = function() {
-              $scope.tabContainer = $el[0].querySelector('.spacer').querySelector('ul.nav.nav-tabs');
+              $scope.tabContainer = $el[0].querySelector('.spacer ul.nav-tabs');
               if(!$scope.tabContainer)return;
+
+              var autoRecalc = $scope.autoRecalculate ? $scope.autoRecalculate === 'true' : scrollableTabsetConfig.autoRecalculate;
+              if(autoRecalc) {
+                var tabsetElement = angular.element($el[0].querySelector('.spacer div'));
+                $scope.$watchCollection(
+                    function () {
+                      return tabsetElement.isolateScope() ? tabsetElement.isolateScope().tabs : false;
+                    },
+                    function (newValues, oldValues) {
+                      $timeout(function () {
+                        $scope.reCalcAll()
+                      });
+                    }
+                );
+              }
 
               var leftNav = angular.element($el[0].querySelector('.left-nav-button'));
               var rightNav = angular.element($el[0].querySelector('.right-nav-button'));
@@ -249,8 +278,25 @@ angular.module('ui.tab.scroll', [])
               if(!$scope.tabContainer)return;
 
               $scope.hideButtons = $scope.tabContainer.scrollWidth <= $scope.tabContainer.offsetWidth;
+              $scope.hideDropDown = showDropDown ? $scope.hideButtons : true;
 
               if(!$scope.hideButtons) {
+
+                if(!$scope.hideDropDown) {
+                  var allTabs = $scope.tabContainer.querySelectorAll('li');
+                  $scope.dropdownTabs = [];
+                  angular.forEach(allTabs, function (tab) {
+                    var ignore = tab.getAttribute("data-tabScrollIgnore");
+                    if(!ignore){
+                      var heading = tab.getAttribute("data-tabScrollHeading");
+                      var tabScope = angular.element(tab).isolateScope();
+                      //push new field to use as title in the drop down.
+                      tabScope.tabScrollTitle = heading ? heading : tabScope.headingElement.textContent;
+                      $scope.dropdownTabs.push(tabScope);
+                    }
+                  });
+                }
+
                 $scope.reCalcSides();
               }
             };
